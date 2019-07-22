5465c1546ef94316b6ad80947ba90a173941e427
Richard <starengram@gmail.com>
2019-07-22 14:46:28 +0800

    set Cells

---
+++ iOSNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleTableViewCell.swift
+//  iOSNews
+//
+//  Created by chang on 2019/7/22.
+//  Copyright © 2019 chang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
