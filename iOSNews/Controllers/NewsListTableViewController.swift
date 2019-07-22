45d3a4693588d94e6daa4e0a887bc0f382a3b067
Richard <starengram@gmail.com>
2019-07-22 15:11:03 +0800

    tableview done

--- iOSNews/Controllers/NewsListTableViewController.swift
+++ iOSNews/Controllers/NewsListTableViewController.swift
@@ -10,6 +10,9 @@ import Foundation
 import UIKit
 
 class NewsListTableViewController: UITableViewController {
+    
+    private var articleListVM: ArticleListViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
@@ -18,8 +21,32 @@ class NewsListTableViewController: UITableViewController {
     private func setupViews() {
         self.navigationController?.navigationBar.prefersLargeTitles = true
         let url = URL(string: "https://newsapi.org/v2/top-headlines?country=tw&apiKey=bc0374957d234e35b24cb86bf279b3c6")!
-        WebService().getArticles(url: url) { _ in
-            
+        
+        WebService().getArticles(url: url) { articles in
+            if let articles = articles {
+                self.articleListVM = ArticleListViewModel(articles: articles)
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
         }
+    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.articleListVM == nil ? 0 : self.articleListVM.numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM.numberOfRowsInSection(section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("ArticleTableViewCell nof found")
+        }
+        
+        let articleVM = self.articleListVM.articleAtIndex(indexPath.row)
+        cell.titleLabel.text = articleVM.title
+        cell.descriptionLabel.text = articleVM.description
+        return cell
     }
 }
